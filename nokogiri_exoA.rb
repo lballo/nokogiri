4548e24d85ad7a1e7b65fb1fb115d5dd259be5aa
laura ballo <laura.ballo1993@gmail.com>
2018-01-18 23:28:21 +0100

    amélioration du code exo 2

--- nokogiri_exoA.rb
+++ nokogiri_exoA.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'   
 
-def get_the_email_of_a_townhal_from_its_webpage
+def get_the_email_of_a_townhal_from_its_webpage #va chercher dans le code html tous les éléments dans les balises font ayant un @
  	page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/vaureal.html"))   
  	element = page.css('tr td p font')
  		element.each do |x|
@@ -14,7 +14,7 @@ end
 
 get_the_email_of_a_townhal_from_its_webpage
 
-def get_all_the_urls_of_val_doise_townhalls
+def get_all_the_urls_of_val_doise_townhalls #sélectionne dans les balises de liens les href et les renvoie dans un tableau
 page2 = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html")) 
 townhalls_url = page2.css('a')
 lien = townhalls_url.each{|link| print link['href'] + " "}
--- nokogiri_exoB.rb
+++ nokogiri_exoB.rb
@@ -2,10 +2,39 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-crypto_name = []
-crypto_value = []
+#METHODE 1 : CODE FONCTIONNEL 
 
-def names_crypto()
+
+PAGE_URL = "https://coinmarketcap.com/all/views/all/"
+def crypto_trader
+    crypto_names = [] #création des tableaux de données
+    crypto_price = []
+    my_hash = Hash.new #création du hash
+    page = Nokogiri::HTML(open(PAGE_URL))
+    i = 0
+    page.css('.currency-name-container').each do |elem|
+        crypto_names << elem.text #scrap des données nom des crypto
+    end
+    page.css('.price').each do |p| #scrap des données prix des crypto
+        crypto_price << p.text
+    end
+    size = crypto_names.length
+    while i < size do #boucle pour ranger tableaux dans le hash
+        my_hash[crypto_names[i]] = crypto_price[i]
+        i += 1 #évolution indice du hash 
+    end
+    puts my_hash
+end
+crypto_trader
+
+=begin
+METHODE 1 : CODE A REPRENDRE: DIFFICULTÉS D'APPEL A DES VARIABLES SITUÉES EN DEHORS DES METHODES
+
+#initialisation des variables en dehors des fonctions
+$crypto_name = []
+$crypto_value = []
+
+def names_crypto #récupère le nom de chacune des cryptomonnaies et les range dans un tableau
  	coin_market_url="https://coinmarketcap.com/all/views/all/"
  	page=Nokogiri::HTML(open(coin_market_url))
  	page.xpath("//td[2]/a").each do |name|
@@ -14,23 +43,27 @@ def names_crypto()
 end 
 
 
-def value_crypto()
+def value_crypto #récupère la valeur de chacune des cryptomonnaies et les range dans un tableau
 	coin_market_url="https://coinmarketcap.com/all/views/all/"
 	page=Nokogiri::HTML(open(coin_market_url))
 	page.xpath("//td[5]/a").each do |value|
-		crypto_value << value.text
+		crypto_value << value.text 
 	end
 end 
 
-def all_value
-size = crypto_name.length
+def all_value #consolidation des deux tableaux dans un hash {clé : valeur}
+size = $crypto_name.length
 all = Hash.new
 i = 0
 while i < size do
-	all[crypto_name[i]] = crypto_value[i]
-	i += 1
-	end
+ 	all[$crypto_name[i]] = $crypto_value[i]
+ 	i += 1
+ 	end
 puts all
 end
 
+$crypto_name = names_crypto
+$crypto_value = value_crypto
+
 all_value()
+=end
